5080a1b75fe1b67882be4b26010f59203750fbf3
KcuD-L <a77787877a@gmail.com>
2026-04-25 21:51:26 +0300

    create entities and enum

---
+++ Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Transactions;
+namespace FinTrack.Entities
+
+{
+    public class User : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public ICollection<Transaction> Transactions { get; set; } = new List<Transaction>();
+    }
+}
---
+++ Enum/TransactionType.cs
@@ -0,0 +1,8 @@
+﻿namespace FinTrack.Enum
+{
+    public enum TransactionType
+    {
+        Income = 1, // Доход
+        Expense = 2 // Расход
+    }
+}
